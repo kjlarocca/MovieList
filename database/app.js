86a397d4d76a840cfd9f46e3849c2e074b7f77eb
KIRSTIN <kwarren1@lssu.edu>
2023-07-17 16:53:08 -0400

    connecting express and knex

---
+++ database/app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/, (req, res) => {
+    res.status(200).send('Response from root route')
+})
+
+app.listen(port, () => console.log(`Express server listening on port ${port}.`))
